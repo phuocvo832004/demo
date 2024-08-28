70669e1f23390e6e5e0d0940313ab187d66e084d
vohuuphuoc <voroberrt@gmail.com>
2024-08-28 22:16:24 +0700

    High Quality Code Standard

--- demo.js
+++ demo.js
@@ -1,11 +1,12 @@
 const isPrime = (num) => {
     if (num <= 1) return false;
-    
-    for (let i = 2; i <= num; i++) {
-      if (num % i === 0) {
-        return i === num;
-      }
+    if (num <= 3) return true;
+  
+    if (num % 2 === 0 || num % 3 === 0) return false;
+  
+    for (let i = 5; i * i <= num; i += 6) {
+      if (num % i === 0 || num % (i + 2) === 0) return false;
     }
-    
-    return false;
+  
+    return true;
   };
